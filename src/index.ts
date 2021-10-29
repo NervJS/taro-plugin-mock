881f9711382c22428dc7454cc38daca43feab560
liu_yang <liu_yang@haici.com>
2021-10-29 17:17:57 +0800

    fix: 修复post请求无法获取请求参数问题 fix #9

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Server from './Server'
+import * as express from 'express'
 import { createMockMiddleware } from './utils'
 
 export default (ctx, pluginOpts) => {
@@ -26,6 +27,7 @@ export default (ctx, pluginOpts) => {
         port,
         host,
         middlewares: [
+          express.json(),
           createMockMiddleware({
             appPath,
             mocks,
